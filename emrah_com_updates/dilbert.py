0c8d52fc64a22734d5e7763ee1325b5b82dde8e4
emrah <emrah@gnu.loc>
2015-02-06 09:33:10 +0200

    Dilbert kodu degisti

--- emrah_com_updates/dilbert.py
+++ emrah_com_updates/dilbert.py
@@ -6,7 +6,7 @@ import re
 import urllib2
 
 # Gunun resminin yolunu ve adini arayacak sablonu olustur
-PATTERN = '<img src="http://dilbert.com/(dyn/str_strip/.*?)"'
+PATTERN = '<img.*?src="http://assets.amuniversal.com/(.*?)"'
 PATTERN = re.compile(PATTERN, re.IGNORECASE)
 # Dilbert blogunu olusturan dosya.
 FILE = '/home/emrah/proje/www_emrah_com/public_html/inc/dilbert.new.html'
@@ -26,15 +26,15 @@ try:
 
     # Dilbert blogunu olusturacak icerigi guncelle.
     block = """
-		<a href="http://www.dilbert.com/"
-		title="Başka karikatürler de görmek istiyorsanız, tıklayınız">
-		<img src="http://www.dilbert.com/%s" alt="" />
-		</a>
-		<br />
-		<span class="h3">
-		<a href="http://www.dilbert.com">From Dilbert.com</a><br />
-		Copyright © Scott Adams, Inc./Dist. by UFS, Inc.
-		</span><br />""" % (link)
+        <a href="http://www.dilbert.com/"
+        title="Başka karikatürler de görmek istiyorsanız, tıklayınız">
+        <img src="http://assets.amuniversal.com/%s" alt="" />
+        </a>
+        <br />
+        <span class="h3">
+        <a href="http://www.dilbert.com">From Dilbert.com</a><br />
+        Copyright © Scott Adams, Inc./Dist. by UFS, Inc.
+        </span><br />""" % (link)
 
     # Dilbert blogunu olusturacak icerigi tasiyan dosyayi guncelle.
     with open(FILE, 'w') as f:
