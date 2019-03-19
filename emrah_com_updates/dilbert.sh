97bc8ab247b2852eae56bfe483e594b67ae820da
emrah <emrah.com@gmail.com>
2019-03-19 09:29:22 +0300

    dilbert update script updated

--- emrah_com_updates/dilbert.sh
+++ emrah_com_updates/dilbert.sh
@@ -2,8 +2,8 @@
 set -e
 
 DEST="$(dirname $0)/../public_html/medya/dilbert.gif"
-SRC=$(curl -s http://dilbert.com/ | \
-      egrep -ohi 'src="http://assets.amuniversal.com/[^"]*' | \
+SRC=$(curl -s https://dilbert.com/ | \
+      egrep -ohi 'src="//assets.amuniversal.com/[^"]*' | \
       head -1 | cut -d '"' -f2)
 
 [ -z "$SRC" ] && exit 1
