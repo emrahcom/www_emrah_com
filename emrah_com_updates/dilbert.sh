5a6d692b361bec97f2e0e26454e35d635384cc69
emrah <emrah.com@gmail.com>
2020-09-13 12:36:09 +0300

    dilbert update script

--- emrah_com_updates/dilbert.sh
+++ emrah_com_updates/dilbert.sh
@@ -3,10 +3,10 @@ set -e
 
 DEST="$(dirname $0)/../public_html/media/dilbert.gif"
 SRC=$(curl -s https://dilbert.com/ | \
-      egrep -ohi 'src="//assets.amuniversal.com/[^"]*' | \
+      egrep -ohi 'assets.amuniversal.com/[^"]*' | \
       head -1 | cut -d '"' -f2)
 
 [ -z "$SRC" ] && exit 1
-SRC="https:$SRC"
+SRC="https://$SRC"
 
 wget -q -O $DEST $SRC
